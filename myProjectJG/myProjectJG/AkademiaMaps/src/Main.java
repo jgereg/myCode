d86044712c7ab3e6c0d37fded36711d9b87ed5da
jgerega <geregakubo@gmail.com>
2024-11-02 19:36:11 +0100

    Tvoj popis zmien

---
+++ myProjectJG/myProjectJG/AkademiaMaps/src/Main.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class Mapa {
+//    public static void main(String[] args) {
+//        ArrayList<String> names = new ArrayList<>();
+//        names.add("Jakub");
+//        names.add("Barbora");
+//        names.add("Charlotte");
+//
+//        for (String name : names){
+//            System.out.println(name);
+//        }
+//        System.out.println(names.contains("Jakub"));
+//        System.out.println(names.contains("Jakubko"));
+//        System.out.println(names.contains("Charlotte"));
+//    }
+
+    public static void main(String[] args) {
+
+        HashMap<String, String> dictionary = new HashMap<>();
+        dictionary.put("Jablko", "Ovocie, ktore je okruhle");
+        dictionary.put("Cucoriedky", "Ovocie, ktore rastie na krikoch");
+        System.out.println(dictionary);
+
+
+    }
+
+}
